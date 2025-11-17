a60c31ee63ae2d9c92e65ec77138b3985bab286f
Nowshin Nawar <68994761+Nowshin73@users.noreply.github.com>
2025-11-18 02:48:56 +0600

    completed

--- solution.ts
+++ solution.ts
@@ -1,5 +1,4 @@
-//Problem 1
-const formatValue = (input: string | number | boolean) => {
+const formatValue = (input: string | number | boolean): (string | number | boolean) => {
     if (typeof input === "string") {
         return input.toUpperCase();
     }
@@ -9,12 +8,11 @@ const formatValue = (input: string | number | boolean) => {
     else if (typeof input === "boolean") {
         return !input;
     }
+    else { 
+        return "Invalid Input";
+    }
 }
-console.log(formatValue('hello'));
-console.log(formatValue(5));
-console.log(formatValue(false));
 
-// Problem 2
 const getLength = (value: unknown): number => {
     if (typeof value === "string") {
         return value.length;
@@ -22,11 +20,11 @@ const getLength = (value: unknown): number => {
     else if (Array.isArray(value) === true) {
         return value.length;
     }
+    else {
+         return 0;
+    }
 }
-console.log(getLength('typescript'));
-console.log(getLength([10, 20, 30, 40]));
 
-// Problem 3
 class Person {
     name: string;
     age: number;
@@ -35,62 +33,65 @@ class Person {
         this.age = age;
     }
 
-    getDetails() {
-        return `Name: ${this.name}, Age: ${this.age}`;
+    getDetails(): string {
+        return `'Name: ${this.name}, Age: ${this.age}'`;
     }
 }
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
 
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
-
-// Problem 4
-const filterByRating = (input: { title: string, rating: number }[]) => {
-    return input.filter((i) => (i.rating >= 4 && i.rating <= 5) && i);
+const filterByRating = (input: { title: string, rating: number }[]):{ title: string, rating: number }[] => {
+    
+    return input.filter((i) => i.rating >= 4 && i.rating <= 5);
 }
-const books = [
-    { title: 'Book A', rating: 4.5 },
-    { title: 'Book B', rating: 3.2 },
-    { title: 'Book C', rating: 5.0 },
-];
 
-console.log(filterByRating(books));
-
-//Problem 5
-const filterActiveUsers = (input: { id: number, name: string, email: string, isActive: boolean }[]) => {
-    return input.filter((i) => i.isActive === true && i);
+const filterActiveUsers = (input: { id: number, name: string, email: string, isActive: boolean }[]):{ id: number, name: string, email: string, isActive: boolean }[] => {
+    return input.filter((i) => i.isActive === true);
 }
-const users = [
-    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-];
 
-console.log(filterActiveUsers(users));
-
-//Problem 6
 interface Book {
     title: string;
     author: string;
     publishedYear: number;
     isAvailable: boolean;
 }
-const printBookDetails =(input: Book) =>{
-    const {title, author, publishedYear, isAvailable} = input;
+const printBookDetails =(book: Book) =>{
+    const {title, author, publishedYear, isAvailable} = book;
     console.log(`Title: ${title}, Author: ${author}, Published: ${publishedYear}, Available: ${isAvailable}`);
 }
-const myBook: Book = {
-  title: 'The Great Gatsby',
-  author: 'F. Scott Fitzgerald',
-  publishedYear: 1925,
-  isAvailable: true,
-};
 
-printBookDetails(myBook);
+const getUniqueValues = (Array1: (string | number)[], Array2: (string | number)[]):(string | number)[] =>{
+const jointArr: (string | number)[] = [];
 
-//problem 7
+  const checkUnique = (value: string | number): boolean => {
+    for (let i = 0; i < jointArr.length; i++) {
+      if (jointArr[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  };
 
-const getUniqueValues = (input1:number[],input2:number[]): number[] =>{
+  for (let i = 0; i < Array1.length; i++) {
+    if (!checkUnique(Array1[i])) {
+      jointArr.push(Array1[i]);
+    }
+  }
 
+  for (let i = 0; i < Array2.length; i++) {
+    if (!checkUnique(Array2[i])) {
+      jointArr.push(Array2[i]);
+    }
+  }
+
+  return jointArr;
 } 
+
+ const  calculateTotalPrice = 
+ (products:
+     { name: string,
+       price: number, 
+       quantity: number,
+       discount?:number 
+    }[]
+    ):number =>{
+    return products.reduce((sum,product)=> sum + product.price * product.quantity -( product.discount?( (product.discount*product.price*product.quantity)/100): 0),0)
+ }
